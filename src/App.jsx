0527db344a8eba53587135f8add9f91acc1d7167
Sunny Mishra <sunnymishra9007689708@gmail.com>
2024-09-13 18:40:24 +0530

    added favicon and background color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TaskList from './Components/TaskList';
 
 const App = () => {
   return (
-    <div className='h-screen w-screen bg-slate-500 flex flex-col gap-3 items-center pt-5'>
+    <div className='h-screen w-screen bg-black flex flex-col gap-3 items-center pt-5'>
       <DataInputBox/>
       <TaskList/>
     </div>
